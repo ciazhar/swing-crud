1e4bde8ad06a21b17bbd379d31a12f5d1b6b06b9
ciazhar <daisetsunakama@gmail.com>
2017-05-25 12:30:33 +0700

    gui

--- src/com/ciazhar/FormBarang/Main.java
+++ src/com/ciazhar/FormBarang/Main.java
@@ -3,7 +3,9 @@ package com.ciazhar.FormBarang;
 public class Main {
 
     public static void main(String[] args) {
-
+        FormBarang formBarang = new FormBarang();
+        
+        formBarang.setVisible(true);
     }
     
 }
